4eb1a7ab4d555d655858fbc08286f7a20ec66822
moniquevith <moniquevith1@gmail.com>
2023-05-16 10:57:41 +1000

    added notes on data types in js

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log('Hello World')
+console.log('Hello' + 'World')
+console.log('Hello World' [10])
+console.log('Hello World'.length)
+
+// always gives last letter of string
+let str = "Monique"
+console.log(str[str.length - 1])
+
+
+
+
+
+
+
+
+
+
